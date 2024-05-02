53aabdcc4fd3bcd47670e036459c69ed5cb10849
MZain3028 <72807201+MZain3028@users.noreply.github.com>
2024-05-02 10:25:52 +0500

    Create client.py
    version 1, Creates 100 client threads

---
+++ client.py
@@ -0,0 +1,13 @@
+import requests
+import threading
+from time import sleep
+
+def req():
+    url     = 'http://localhost:6000'
+    payload = { 'key' : 'val' }
+    headers = {}
+    res = requests.get(url, data=payload, headers=headers)
+
+for i in range(100):
+    threading.Thread(target = req).start()
+    sleep(2)
